d269c3dd3a431a601c008f350053883f74f8c898
dlobosm <diegolobos@MacBook-Pro-de-Diego.local>
2023-06-27 18:12:43 -0400

    avance app

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+5.times do
+  Character.create(
+    name: Faker::Movies::HarryPotter.character,
+    location: Faker::Movies::HarryPotter.location,
+    house: Faker::Movies::HarryPotter.house
+  )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
